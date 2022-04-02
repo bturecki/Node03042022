354166757e36a7bf67fe8e42cdcfe6d0bd8ee5e7
Bartosz Turecki <bartosz.turecki@gmail.com>
2022-04-02 09:41:10 +0200

    Main

--- Node/Program.cs
+++ Node/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Node;
+
+Catalog music = new Catalog();
+Node.File jackson = new Node.File();
+
+INode proxy = new NodeProxy();
+proxy.Name = "good";
+
+music.List = new List<INode>() { jackson };
+Console.WriteLine($"File added to catalog");
